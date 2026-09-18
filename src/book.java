9eddaaa3ca5f8d483c630ee6cea6ecfe66ee5de9
AryanS19 <aryan.sharma.190805@gmail.com>
2026-09-18 23:04:09 +0530

    Book.java

--- src/book.java
+++ src/book.java
@@ -1,1 +1,44 @@
+public class Book {
 
+    private String bookId;
+    private String title;
+    private String author;
+    private boolean available;
+
+    public Book(String bookId, String title, String author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.available = true;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public void displayBook() {
+        System.out.println("----------------------------------------");
+        System.out.println("Book ID     : " + bookId);
+        System.out.println("Title       : " + title);
+        System.out.println("Author      : " + author);
+        System.out.println("Availability: " +
+                (available ? "Available" : "Issued"));
+        System.out.println("----------------------------------------");
+    }
+}
